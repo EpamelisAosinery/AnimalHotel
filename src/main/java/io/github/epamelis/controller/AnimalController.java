5a6582d356a3ace6e05aac7a8bdbcca207ac8b6f
Hoa Ho <hxho@hawkmail.hfcc.edu>
2021-07-04 15:59:24 -0400

    init project

---
+++ src/main/java/io/github/epamelis/controller/AnimalController.java
@@ -0,0 +1,11 @@
+package io.github.epamelis.controller;
+
+// confirm scope trc
+// ghi doc r m muon feature gi dua tren scope do
+// tu do m design db
+// xong di tu entity -> repository -> service -> controller
+// chỉ di tu controller xuong khi m da co db r
+
+// gơio viet 1 cai  PRD
+public class AnimalController {
+}
---
+++ src/test/java/io/github/epamelis/AnimalsHotelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.epamelis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsHotelSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
